669f50395e1abf5ed344d1d7ce42d0f8efe89c94
Oleksii Peregonchuk <oleksij.peregonchuk@gmail.com>
2022-09-27 20:27:02 +0300

    fix

---
+++ src/test.js
@@ -0,0 +1,10 @@
+const string = 'Welcome to the Javascript';
+let reversedString = '';
+
+for (let i = string.length - 1; i >= 0; i -= 1) {
+    reversedString += string[i];
+}
+
+console.log(reversedString);
+
+
--- test.js
+++
@@ -1,6 +0,0 @@
-const greet = 'Welcome to the Javascript';
-console.log(greet);
-
-const greet1 = greet.split(" ").reverse();
-
-
